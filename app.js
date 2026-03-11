5aa87b963028f37e838d86ab928137b69d9fdf60
Muhammad Asim Afridi <email-used-on-github@gmail.com>
2026-03-11 11:59:47 +0500

    Update Project

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 let count = 0;
 // Increment function where the counter will increase.
 function increment (){
-    count++;
-    console.log(count);
+    count++;    // Increment
+    console.log(count); // Output to the console.
     document.querySelector("h1").textContent = count;
 }
 // Decrement function where the counter will decrease.
 function decrement (){
-    count--;
-    console.log(count);
+    count--;    // Decrement
+    console.log(count); // Output to the console.
     document.querySelector("h1").textContent = count;
 }
 // Reset function where the counter will reset and the value will again start from 0.
 function reset (){
-    count = 0;
-    console.log(count);
+    count = 0;    // Reset
+    console.log(count); // Output to the console.
     document.querySelector("h1").textContent = count;
 }
